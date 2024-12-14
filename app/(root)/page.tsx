b9c233503e84df0c222e01ce48a24f344c03ab94
조승주 <tmdwn12512@gmail.com>
2024-12-14 17:39:05 +0900

    deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -13,8 +12,6 @@ export default async function Home({searchParams}:
   const params = {search: query || null};
   // const posts = await client.fetch(STARTUPS_QUERY)
   const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
-
-  const session = await auth()
 
   return (
     <>
@@ -33,7 +30,7 @@ export default async function Home({searchParams}:
         
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
